76788b01d41bde9437d895292df7e02de8c322b3
Hayden Worden <haydenworden@outlook.com>
2018-08-02 19:57:19 +0100

    Footer, header, signup and index.php setup

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include 'header.php' ?>
+<section>
+  <h1>Welcome!</h1>
+</section>
+
+  <section>
+    <div class="container">
+      <div class="row">
+
+        </div>
+      </div>
+    </div>
+  </section>
+<?php include 'footer.php' ?>
