99606b86b4bf9400f8877092f63bcc85d70d6ec7
Hayden Worden <haydenworden@outlook.com>
2018-08-03 10:32:03 +0100

    Showcase changed.

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 
 </head>
 <body>
+  <section id='showcase'>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
         <a class="navbar-brand" href="index.php">Blog.com</a>
