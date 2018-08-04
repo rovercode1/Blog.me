cefa01c7823f0afb1d1805acd1f341f71df3baef
Hayden Worden <haydenworden@outlook.com>
2018-08-04 12:34:30 +0100

    Section for features

--- front-page.php
+++ front-page.php
@@ -5,8 +5,23 @@
   <h1>Create your blog today!</h1>
 </section>
 <section id='why_us'>
-  <div class="text-field">
-    <h1 id='main-text'>Powerful Features </h1>
-    <h1>for Your Website</h1>
+  <div class="container">
+    <div class="text-field">
+      <h1 id='main-text'>Powerful Features </h1>
+      <h1>for Your Website</h1>
+    </div>
+    <div id="icons">
+      <div class="row">
+        <div class="col-lg-4">
+          <img src="assets/img/desktop.png" alt="">
+        </div>
+        <div class="col-lg-4">
+          <img src="assets/img/phone.png" alt="">
+        </div>
+        <div class="col-lg-4">
+          <img src="assets/img/seo.png" alt="">
+        </div>
+      </div>
+    </div>
   </div>
 </section>
