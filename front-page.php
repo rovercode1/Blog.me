a6ea8cb0d561caf979950638e334bececcebab9f
Hayden Worden <haydenworden@outlook.com>
2018-08-04 11:47:59 +0100

    Front page sections added

--- front-page.php
+++ front-page.php
@@ -2,4 +2,11 @@
   include_once 'includes/dbh.inc.php';
 ?>
 <section id='showcase'>
+  <h1>Create your blog today!</h1>
+</section>
+<section id='why_us'>
+  <div class="text-field">
+    <h1 id='main-text'>Powerful Features </h1>
+    <h1>for Your Website</h1>
+  </div>
 </section>
