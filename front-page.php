e39d730450b55075d189c973bb162174b828ad14
Hayden Worden <haydenworden@outlook.com>
2018-08-04 12:56:12 +0100

    Get started button added

--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@
 ?>
 <section id='showcase'>
   <h1>Create your blog today!</h1>
+  <a class="btn "href="#">Get started!</a>
 </section>
 <section id='features'>
   <div class="container">
@@ -14,12 +15,15 @@
       <div class="row">
         <div class="col-lg-4">
           <img src="assets/img/desktop.png" alt="">
+          <p>Beautiful Blogs</p>
         </div>
         <div class="col-lg-4">
           <img src="assets/img/phone.png" alt="">
+          <p>Mobile Friendly</p>
         </div>
         <div class="col-lg-4">
           <img src="assets/img/seo.png" alt="">
+          <p>Industry-leading seo</p>
         </div>
       </div>
     </div>
