f6ff4925c9b29aeebec242ad6392d55722c38d75
Hayden Worden <haydenworden@outlook.com>
2018-08-04 18:12:28 +0100

    section for spotlight blogs

--- front-page.php
+++ front-page.php
@@ -29,7 +29,7 @@
     </div>
   </div>
 </section>
-<section >
+<section>
   <div class="container">
     <div id='features'>
       <h3>Unlease your creativity with our Uniquie Domains, Blog Customizer and <span id='more'>much more!</span> </h3>
@@ -39,6 +39,18 @@
           <h2 id='freedom'>Your Freedom</h2>
         </div>
           <img src="assets/img/blog.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</section>
+<section >
+  <div class="container">
+    <h2>Spotlight Blogs</h2>
+    <div id="blogs">
+      <div class="row">
+        <div class="col-lg-4">
+          (**Blogs go here**)
+        </div>
       </div>
     </div>
   </div>
