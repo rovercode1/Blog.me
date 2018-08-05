d2a7a52a9e0aa8c34b29fb6b33b1f717d67c38d5
Hayden Worden <haydenworden@outlook.com>
2018-08-05 11:44:53 +0100

    Show all blogs on index.php when user is signed in

--- main.php
+++ main.php
@@ -1,9 +1,32 @@
-
 <section>
   <div class="container">
     <div class="row">
-        <h1>YOU ARE LOGEGD IN!</h1>
-      </div>
+    <?php
+      if (isset($_SESSION['u_id'])) {
+        $fullUrl = "http://$_SERVER[HTTP_HOST]";
+        $blogs = 'SELECT * FROM blogs';
+        $blogResult = mysqli_query($conn, $blogs);
+        $blogresultCheck = mysqli_num_rows($blogResult);
+        // If there are no results in the database...
+        if ($blogresultCheck < 1) {
+        ?>
+          <h1>There are no blogs.</h1>
+        <?php
+        }else{
+          //Push the result in an array = $row
+          while ($row = mysqli_fetch_assoc($blogResult)) {?>
+            <div class="col-lg-6 col-md-6">
+              <a href="blogs.php <?php echo '?blog='. $row['post_id']?>" ><h4><?php echo $row['post_title'] ?> </h4></a>
+              <p> <?php echo $row['post_body'] ?> </p>
+              <p> <?php echo $row['post_author'] ?> </p>
+              <p> <?php echo $row['post_date'] ?> </p>
+            </div>
+          <?php }
+          }
+        ?>
+        <?php
+      }
+    ?>
     </div>
   </div>
 </section>
