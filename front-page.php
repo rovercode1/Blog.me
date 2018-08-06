3c42062be4121ee39a4db60fb0fadb130e32f5b7
Hayden Worden <haydenworden@outlook.com>
2018-08-06 14:50:27 +0100

    Testimonal section added

--- front-page.php
+++ front-page.php
@@ -43,7 +43,7 @@
     </div>
   </div>
 </section>
-<section >
+<section>
   <div class="container">
     <h2>Spotlight Blogs</h2>
     <div id="blogs">
@@ -51,6 +51,48 @@
         <div class="col-lg-4">
           (**Blogs go here**)
         </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section>
+  <div class="container">
+    <h2>Testimonals</h2>
+    <div id='testimonial'>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="testimonial-box">
+              <h1>[USERNAME]</h1>
+              <p>Review</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+          <div class="testimonial-box">
+            <h1>[USERNAME]</h1>
+            <p>Review</p>
+          </div>
+          </div>
+          <div class="carousel-item">
+          <div class="testimonial-box">
+            <h1>[USERNAME]</h1>
+            <p>Review</p>
+          </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
       </div>
     </div>
   </div>
