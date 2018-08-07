7219da8073b81b7a7ce235d834be32b18758d98b
Hayden Worden <haydenworden@outlook.com>
2018-08-07 16:10:55 +0100

    Display users blogs on profile

--- profile.php
+++ profile.php
@@ -15,8 +15,9 @@ if (isset($_SESSION['u_id'])) {
   }else{
     $row = mysqli_fetch_assoc($result);?>
     <div class="container-fluid">
-      <div id="user-box">
-        <p><?php echo $row['user_uid']?></p>
+      <div class='container' id="user-box">
+        <h1> <em><?php echo $row['user_uid']?></em></h1>
+        <h6> <?php echo $row['user_about'] ?> </h6>
         <?php if ($_SESSION['u_id'] == $row['user_id']): ?>
           <form class="" action="includes/user/delete_profile.inc.php  <?php echo '?user='. $row['user_id']?>" method="post">
             <button class='btn btn-danger'type="submit" name="submit">Delete</button>
@@ -24,9 +25,34 @@ if (isset($_SESSION['u_id'])) {
         <a class='btn btn-warning' href="user_update.php <?php echo '?user='. $row['user_id']?>" >Update</a>
         <?php endif; ?>
       </div>
-      <div id="user-blogs">
-        
-      </div>
+      <?php
+        // Find the blog in database
+        $blogs  ="SELECT * FROM blogs WHERE post_author =$user_id";
+        // result = what is found in the database
+        $Blogresult = mysqli_query($conn, $blogs);
+        $BlogresultCheck = mysqli_num_rows($Blogresult);
+        // If there are no results in the database...
+        if ($BlogresultCheck < 1) {
+          ?>
+            <p>Blog not found.</p>
+          <?php }
+          else{
+            while ($row = mysqli_fetch_assoc($Blogresult)) {
+              ?>
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div id="user-blogs">
+                    <a href="blogs.php <?php echo '?blog='. $row['post_id']?>"><h2> <?php echo $row['post_title'] ?> </h2></a>  
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <?php
+            }?>
+            <?php
+          }
+       ?>
     </div>
     <?php
     }
