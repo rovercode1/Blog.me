fae0d981bacc7b0aeaa68523bc5f9ed11bf04624
Hayden Worden <haydenworden@outlook.com>
2018-08-07 11:16:16 +0100

    Show author of blog.

--- blogs.php
+++ blogs.php
@@ -20,16 +20,27 @@ include 'includes/dbh.inc.php';
         <p>Blog not found.</p>
       <?php
     }else{
-      $row = mysqli_fetch_assoc($result)
+      $row = mysqli_fetch_assoc($result);
+      $user_id = $row['post_author'];
+      $sql  ="SELECT * FROM `users` WHERE `user_id` = $user_id";
+      // result = what is found in the database
+      $Userresult = mysqli_query($conn, $sql);
+      $UserresultCheck = mysqli_num_rows($Userresult);
+      // If there are no results in the database...
+      if ($UserresultCheck < 1) {
+        header("Location: http://localhost/project-website/index.php?user=nouser");
+        exit();
+      }else{
+        $Userrow = mysqli_fetch_assoc($Userresult);
       ?>
 
       <h4><?php echo $row['post_title'] ?> </h4>
       <p> <?php echo $row['post_body'] ?> </p>
-      <p>Posted by <a href="#"> <em><?php echo $row['post_author'] ?></em></a></p>
+      <p>Posted by <a href="#"> <em><?php echo $Userrow['user_uid'] ?></em></a></p>
       <p> <?php echo $row['post_date'] ?> </p>
       <div id="buttons">
         <a class='btn btn-primary'href="http://localhost/project-website">Back</a>
-        <?php if ($_SESSION['u_uid'] == $row['post_author']): ?>
+        <?php if ($_SESSION['u_id'] == $row['post_author']): ?>
           <form class="" action="includes/blogs/delete_blog.inc.php  <?php echo '?blog='. $row['post_id']?>" method="post">
             <button class='btn btn-danger'type="submit" name="submit">Delete</button>
           </form>
@@ -38,11 +49,11 @@ include 'includes/dbh.inc.php';
       </div>
     </div>
     <?php
+    }
   }
-}
-else{
-  header("Location: ../index.php");
-  exit();
+}else{
+    header("Location: index.php?loggedin=false");
+    exit();
 }
 include 'footer.php' ?>
 </div>
