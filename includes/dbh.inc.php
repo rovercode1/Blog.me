1aec747c6c399c94dd0cbdb84bf13fb0013a2b58
Hayden Worden <haydenworden@outlook.com>
2018-08-08 09:55:35 +0100

    PDO setup

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -9,4 +9,11 @@ $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=blog_database', $dbUsername, $dbPassword);
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
