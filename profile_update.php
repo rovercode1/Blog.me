2f4cdc7569cef09e434fd6b767425c0c13a9f399
Hayden Worden <haydenworden@outlook.com>
2018-08-09 09:17:56 +0100

    Profile update form

--- profile_update.php
+++ profile_update.php
@@ -18,57 +18,43 @@ if (isset($_SESSION['u_id'])) {
 <section>
   <div class="container-fluid">
     <div class='container' id="user-box">
-      <h1> <em><?php echo $row['user_uid']?></em></h1>
-      <h6> <?php echo $row['user_about'] ?> </h6>
-      <?php if ($_SESSION['u_id'] == $row['user_id']): ?>
+      <form class="" action="includes/users/update_profile.inc.php" method="post">
+        <input id='uid-edit'class='form-control'type="text" name="uid" value="<?php echo $row['user_uid']?>">
+        <input type="text" id='about-edit' class='form-control'name="about" value="<?php echo $row['user_about'] ?>">
+        <div class="collapse" id="moreSettings">
+          <div class="card card-body">
+            <label>First name</label>
+            <input class='form-control'type="text" name="first" value="<?php echo $row['user_first'] ?>">
+            <label>Last name</label>
+            <input class='form-control'type="text" name="last" value="<?php echo $row['user_last'] ?>">
+            <label>Email</label>
+            <input class='form-control'type="text" name="email" value="<?php echo $row['user_email'] ?>">
+          </div>
+        </div>
+        <button class='btn btn-primary btn-sm'type="submit" name="submit">Update</button>
+      </form>
+      <a id="fullSettings" data-toggle="collapse" href="#moreSettings" role="button" aria-expanded="false" aria-controls="moreSettings">
+        Full Settings
+      </a>
         <div class="buttons">
-          <form class="" action="includes/user/delete_profile.inc.php  <?php echo '?user='. $row['user_id']?>" method="post">
+          <form action="includes/user/delete_profile.inc.php  <?php echo '?user='. $row['user_id']?>" method="post">
             <button class='btn btn-danger btn-sm'type="submit" name="submit">Delete</button>
           </form>
 
           <a class='btn btn-warning btn-sm' href="profile.php <?php echo '?user='. $row['user_id']?>" >Go Back</a>
         </div>
-      <?php endif; ?>
+
     </div>
-    <?php
-      // Find the blog in database
-      $blogs  ="SELECT * FROM blogs WHERE post_author =$user_id";
-      // result = what is found in the database
-      $Blogresult = mysqli_query($conn, $blogs);
-      $BlogresultCheck = mysqli_num_rows($Blogresult);
-      // If there are no results in the database...
-      if ($BlogresultCheck < 1) {
-        ?>
-          <h2 class='text-center' style="margin-bottom: 30px;">Blog(s) not found.</h2>
-        <?php }
-        else{
-          while ($row = mysqli_fetch_assoc($Blogresult)) {
-            ?>
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-12">
-                  <a id='blog-link' href="blogs.php <?php echo '?blog='. $row['post_id']?>">
-                    <div id="user-blogs">
-                      <h4> <?php echo $row['post_title'] ?> </h4>
-                      <p class='post_body'> <?php echo $row['post_body'] ?> </p>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </div>
             <?php
-          }?>
-          <?php
+          }else{
+            header("Location: index.php?user=error");
+          }
         }
      ?>
   </div>
 </section>
 <?php
     }else{
-      header("Location: index.php?user=error");
-    }
-  }
-}else{
   header("Location: index.php?user=notloggedin");
 }
 include 'footer.php';
