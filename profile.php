9aefca66e7b7517df461f46a836ea4173f1a1786
Hayden Worden <haydenworden@outlook.com>
2018-08-15 13:17:05 +0100

    Paging added user profile

--- profile.php
+++ profile.php
@@ -27,35 +27,70 @@ if (isset($_SESSION['u_id'])) {
         </div>
       </div>
       <?php
-        // Find the blog in database
-        $blogs  ="SELECT * FROM blogs WHERE post_author =$user_id";
+        $user_id = $_GET['user'];
+        $sql = "SELECT * FROM blogs WHERE `post_author` = $user_id";
         // result = what is found in the database
-        $Blogresult = mysqli_query($conn, $blogs);
-        $BlogresultCheck = mysqli_num_rows($Blogresult);
+        $result = mysqli_query($conn, $sql);
+        $total = mysqli_num_rows($result);
         // If there are no results in the database...
-        if ($BlogresultCheck < 1) {
+        if ($total < 1) {
           ?>
-            <h2 class='text-center' style="margin-bottom: 30px;">Blog(s) not found.</h2>
-          <?php }
-          else{
-            while ($row = mysqli_fetch_assoc($Blogresult)) {
+            <p>Blog not found.</p>
+          <?php
+        }else{
+            $limit = 4;
+            $pages = ceil($total / $limit);
+            $page = min($pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
+                'options' => array(
+                    'default'   => 1,
+                    'min_range' => 1,
+                ),
+            )));
+            // Calculate the offset for the query
+            $offset = ($page - 1)  * $limit;
+
+            // Some information to display to the user
+            $start = $offset + 1;
+              //returns the lowest value in that array
+            $end = min(($offset + $limit), $total);
+            // The "back" link
+            $profileUrl = 'http://localhost/project-website/profile.php?user=';
+            $prevlink = ($page > 1) ? '<a href="'.$profileUrl.$user_id.'&page=1title="First page">&laquo;</a> <a href=" '.$profileUrl.$user_id.'&page=' . ($page - 1).'" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+
+            // The "forward" link
+            $nextlink = ($page < $pages) ? '<a href="'.$profileUrl.$user_id.'&page=' . ($page + 1).'" title="Next page">&rsaquo;</a> <a href="'.$profileUrl.$user_id.'&page=' . ($page + 1).'" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+
+            $blogs ="SELECT * FROM blogs WHERE `post_author` = $user_id LIMIT $limit OFFSET $offset";
+            $Blogresult = mysqli_query($conn, $blogs);
+            $Blogtotal = mysqli_num_rows($Blogresult);
+            // If there are no results in the database...
+            if ($Blogtotal < 1) {
               ?>
-              <div class="container">
-                <div class="row">
-                  <div class="col-lg-12">
-                    <a id='blog-link' href="blogs.php <?php echo '?blog='. $row['post_id']?>">
-                      <div id="user-blogs">
-                        <h4> <?php echo $row['post_title'] ?> </h4>
-                        <p class='post_body'> <?php echo $row['post_body'] ?> </p>
-                      </div>
-                    </a>
-                  </div>
+                <p>Blog not found.</p>
+              <?php
+            }else{
+
+          while ($row = mysqli_fetch_assoc($Blogresult)) {
+            ?>
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <a id='blog-link' href="blogs.php <?php echo '?blog='. $row['post_id']?>">
+                    <div id="user-blogs">
+                      <h4> <?php echo $row['post_title'] ?> </h4>
+                      <p class='post_body'> <?php echo $row['post_body'] ?> </p>
+                    </div>
+                  </a>
                 </div>
               </div>
-              <?php
-            }?>
+            </div>
             <?php
           }
+          echo '<div id="paging"><p>', $prevlink, ' Page ', $page, ' of ', $pages, ' pages, displaying ', $start, '-', $end, ' of ', $total, ' results ',
+          $nextlink, ' </p></div>';
+        }
+
+    }
        ?>
     </div>
     <?php
