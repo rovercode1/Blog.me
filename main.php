ecf9aa46705385e1913c422e739ea8a7c37feb4a
Hayden Worden <haydenworden@outlook.com>
2018-08-16 19:44:33 +0100

    Display 'Most Recent' with the most recent blogs.

--- main.php
+++ main.php
@@ -1,7 +1,6 @@
 <section id='main-site'>
   <div class="container">
     <!-- <div id="content"> -->
-
       <div id='featured'>
         <div class="blog_desc" id='featured'>
           <div class="row">
@@ -88,49 +87,59 @@
           <h1 class='main-header'>Most Recent</h1>
         <?php
         if (isset($_SESSION['u_id'])) {
-          $fullUrl = "http://$_SERVER[HTTP_HOST]";
-          $blogs = 'SELECT * FROM blogs';
-          $blogResult = mysqli_query($conn, $blogs);
-          $blogresultCheck = mysqli_num_rows($blogResult);
-          // If there are no results in the database...
-          if ($blogresultCheck < 1) {
-            ?>
-              <h1>There are no blogs.</h1>
+          $sql = "SELECT * FROM blogs";
+          $result = mysqli_query($conn, $sql);
+          $total = mysqli_num_rows($result);
+            if ($total < 1) {?>
+              <p>Blog not found.</p>
             <?php
-          }else{
-            //Push the result in an array = $row
-            while ($row = mysqli_fetch_assoc($blogResult)) {
-              $user_id = $row['post_author'];
-              $sql  ="SELECT * FROM `users` WHERE `user_id` = $user_id";
-              // result = what is found in the database
-              $Userresult = mysqli_query($conn, $sql);
-              $UserresultCheck = mysqli_num_rows($Userresult);
-              // If there are no results in the database...
-              if ($UserresultCheck < 1) {
-                header("Location: http://localhost/project-website/index.php?user=nouser");
-                exit();
               }else{
-                $Userrow = mysqli_fetch_assoc($Userresult);
-                ?>
-                <div class="col-lg-12 col-md-12">
-                  <div class="blog_desc">
+            $limit = 3;
+            $offset = $total + 1 - $limit;
+            $blogs ="SELECT * FROM blogs ORDER BY `post_id`DESC LIMIT $limit ";
+            $Blogresult = mysqli_query($conn, $blogs);
+            $Blogtotal = mysqli_num_rows($Blogresult);
+              if ($Blogtotal < 1) {  ?>
+              <p>Blog not found.</p>
+              <?php
+              }else{
+                while ($row = mysqli_fetch_assoc($Blogresult)) {
+                $user_id = $row['post_author'];
+                $sql  ="SELECT * FROM `users` WHERE `user_id` = $user_id";
+                // result = what is found in the database
+                $Userresult = mysqli_query($conn, $sql);
+                $UserresultCheck = mysqli_num_rows($Userresult);
+                // If there are no results in the database...
+                  if ($UserresultCheck < 1) {
+                    header("Location: http://localhost/project-website/index.php?user=nouser");
+                    exit();
+                  }else{
+                    $Userrow = mysqli_fetch_assoc($Userresult);
+                    ?>
+                    <div class="col-lg-12 col-md-12">
+                    <div class="blog_desc">
                     <a href="blogs.php <?php echo '?blog='. $row['post_id']?>" ><h4><?php echo $row['post_title'] ?> </h4></a>
-                    <p>Posted by <a href="http://localhost/project-website/profile.php?user=<?php echo $row['post_author']?>"> <em><?php echo $Userrow['user_uid'] ?></em></a></p>
-                    <p><?php echo $row['post_date'] ?></p>
-                  </div>
-                </div>
-              <?php }
-            }
-            ?>
-          </div>
-          <div class="col-lg-4 col-md-4">
-            <div class="col-lg-12">
-              <h1 class='main-header'>To The Point</h1>
-
+                    <p id='post_author'>Posted by <a href="http://localhost/project-website/profile.php?user=<?php echo $row['post_author']?>"> <em><?php echo $Userrow['user_uid'] ?></em></a></p>
+                    <p id='post_date'><?php echo $row['post_date'] ?></p>
+                    </div>
+                    </div>
                 <?php }
-              }?>
-            </div>
-          </section>
+                }
+              }
+            }
+          }else{
+            header("Location: ../../index.php");
+            exit();
+          }?>
+    </div>
+    <div class="col-lg-4 col-md-4">
+      <div class="col-lg-12">
+        <h1 class='main-header'>To The Point</h1>
+
       </div>
     </div>
+
+<!-- Row/Container/Main-site -->
+    </div>
   </div>
+</section>
