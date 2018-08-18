54391a5f2fcfb1d98a85e4428d282e1f229c69b9
Hayden Worden <haydenworden@outlook.com>
2018-08-18 18:43:20 +0100

    Add user privileges. 0 = user, 1 = poster, 2 = admin,

--- test.php
+++ test.php
@@ -1,6 +1,18 @@
-<?php include 'header.php' ?>
-<form method='POST' action='UploadImage.php' enctype="multipart/form-data">
-  <input type='file' name='file'>
-  <button type="submit" name="submit">Submit</button>
-</form>
-<?php include 'footer.php' ?>
+<?php
+session_start();
+    include_once 'includes/dbh.inc.php';
+     $sql = "SELECT * FROM users";
+     $result = mysqli_query($conn, $sql);
+     while($row = mysqli_fetch_assoc($result)){;
+    $user_id = $row['user_id'];
+    $author = $row['user_uid'];
+    ?>
+      <p> <?php echo $user_id ?> </p>
+      <p> <?php echo $author ?> </p>
+    <?php
+    $priv = "INSERT INTO `user_priv`(`id`, `priv`,`author`) VALUES ($user_id,0,'$author')";
+    mysqli_query($conn, $priv);
+    }
+    header("Location: index.php?=success");
+    exit();
+ ?>
